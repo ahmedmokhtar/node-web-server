b5482b3a9d9435995805caf1f6123e6e6a5f1cb2
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-09-30 20:51:42 +0200

    Adding npm files

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 "# node-web-server" 
-Jus learning how to use Express framework in Node.
+Just learning how to use Express framework in Node.
---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+
+var app = express()
+
+app.get('/', (req, res) => {
+    // res.send('<h1>Hello Express!</h1>')
+    res.send({
+        name: 'Ahmed',
+        age: 44,
+        likes: ['coding', 'eating', 'youtubing']
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.send('About page')
+})
+
+app.get('/bad', (req, res) => {
+    res.send({ errorMessage: 'Unable to handle request.'} )
+})
+
+app.listen(3000)
