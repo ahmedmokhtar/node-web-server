d2c00a965c2b38e143db0e9cbacdc208c8248131
A. Ibrahim <ahmedmokhtar75@gmail.com>
2018-10-01 11:26:41 +0200

    Finished n-5-4

--- server.js
+++ server.js
@@ -1,20 +1,25 @@
 const express = require('express')
+const hbs = require('hbs')
 
 const app = express()
+
+app.set('view engine', 'hbs')
 
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', (req, res) => {
-    // res.send('<h1>Hello Express!</h1>')
-    res.send({
-        name: 'Ahmed',
-        age: 44,
-        likes: ['coding', 'eating', 'youtubing']
+    res.render('home.hbs', {
+        pageTitle: 'Welcome to our site',
+        currentYear: new Date().getFullYear(),
+        message: 'Welcome our visitor'
     })
 })
 
 app.get('/about', (req, res) => {
-    res.send('About page')
+    res.render('about.hbs', {
+        pageTitle: 'About page',
+        currentYear: new Date().getFullYear()
+    })
 })
 
 app.get('/bad', (req, res) => {
