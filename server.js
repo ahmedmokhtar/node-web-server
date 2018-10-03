1a6bc0154146af579ae199985bc8adec65188a2c
Ahmed Ibrahim <me@ahmedmokhtar.com>
2018-10-03 20:50:29 +0200

    n-5-10 -> Finished Challenge, added projects route

--- server.js
+++ server.js
@@ -53,6 +53,13 @@ app.get('/about', (req, res) => {
     })
 })
 
+app.get('/projects', (req, res) => {
+    res.render('projects.hbs', {
+        pageTitle: 'Projects',
+        message: 'Welcome to the projects page'
+    })
+})
+
 app.get('/bad', (req, res) => {
     res.send({ errorMessage: 'Unable to handle request.'} )
 })
